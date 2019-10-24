1ccc67fc6a3976a692b51ce0b2ed6fcd39976e26
BluebirdLyrase <38246181+BluebirdLyrase@users.noreply.github.com>
2019-10-24 11:29:14 +0700

    JQ

--- app.js
+++ app.js
@@ -1,26 +1,69 @@
 console.log("App.js Activated");
 
 
-function Hello(){
-    alert("Hello");
-}
+// function Hello(){
+//     alert("Hello");
+// }
 
-function getDom(){
-var name = document.getElementById('name').innerHTML;
-var major = document.getElementById('major').innerHTML;
-console.log(name+major);
-}
+// function getDom(){
+// var name = document.getElementById('name').innerHTML;
+// var major = document.getElementById('major').innerHTML;
+// console.log(name+major);
+// }
 
-function setDom(){
-    $('#name').empty();
-    $('#major').empty();
-$('#name').append("GRSAW TYTERF");
-$('#major').append("!@#!@$!$!@$");
+// function setDom(){
 
-}
+// document.getElementById('name').innerHTML="sdfsdfsdfsfd";
+// document.getElementById('major').innerHTML="12341141";
+// }
 
-function setList(){
-    var list = document.getElementById('Ilist').value;
+// function setList(){
+//     var list = document.getElementById('Ilist').value;
+//     console.log(list);
+//     document.getElementById('list').innerHTML+='<li>'+list+'</li>';
+//     // $('#list').append('<li>'+list+'</li>');
+// }
+
+
+
+// function Clear(){
+//     console.log('clear');
+//     document.getElementById('list').innerHTML='';
+//     document.getElementById('name').innerHTML="";
+//     document.getElementById('major').innerHTML="";
+// }
+
+$(function () {
+
+    $('#Hello').click(function () {
+        alert("Hello");
+    });
+
+    $('#getDom').click(function () {
+        var name = $('#name').html();
+        var major = $('#major').html();
+        console.log(name + major);
+    });
+
+    $('#setDOM').click(function () {
+        $('#name').empty();
+        $('#major').empty();
+        $('#name').append("GRSAW TYTERF");
+        $('#major').append("!@#!@$!$!@$");
+    });
+
+    $('#setList').click(function () {
+    var list = $('#Ilist').val();
     console.log(list);
     $('#list').append('<li>'+list+'</li>');
-}
+    });
+
+    $('#Clear').click(function () {
+        $('#name').empty();
+        $('#major').empty();
+        $('#list').empty();
+    });
+
+
+
+})
